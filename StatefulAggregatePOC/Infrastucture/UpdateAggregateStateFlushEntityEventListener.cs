ac7bef9b2f62abc100d1865af603c9c12a842bd6
shawaa <andrew.shaw@mhr.co.uk>
2017-06-21 13:17:07 +0100

    Remove brain fart

--- StatefulAggregatePOC/Infrastucture/UpdateAggregateStateFlushEntityEventListener.cs
+++ StatefulAggregatePOC/Infrastucture/UpdateAggregateStateFlushEntityEventListener.cs
@@ -9,32 +9,18 @@ namespace StatefulAggregatePOC.Infrastucture
     {
         public void OnFlushEntity(FlushEntityEvent @event)
         {
-            //Why does this not work when changing an item in a collection? Is @event.Entity a different thing to @event.Session.PersistenceContext.EntityEntries.Keys[0]?
-            //IAggregateState aggregateState = (@event.Entity as IAggregateStatePart)?.AggregateRootState ?? @event.Entity as IAggregateState;
-            //@event.Session.Merge(aggregateState);
+            object entity = @event.Entity;
 
-            ICollection entities = @event.Session.PersistenceContext.EntityEntries.Keys;
+            IAggregateState oldState = entity as IAggregateState;
 
-            IList<IAggregateState> aggregateStates = new List<IAggregateState>();
-
-            foreach (object entity in entities)
+            if (oldState == null)
             {
-                IAggregateState oldState = entity as IAggregateState;
-
-                if (oldState == null)
-                {
-                    continue;
-                }
-
-                IAggregateState newState = oldState.AggregateRoot.GetState();
-
-                aggregateStates.Add(newState);
+                return;
             }
 
-            foreach (IAggregateState aggregateState in aggregateStates)
-            {
-                @event.Session.Merge(aggregateState);
-            }
+            IAggregateState newState = oldState.AggregateRoot.GetState();
+
+            @event.Session.Merge(newState);
         }
     }
 }
